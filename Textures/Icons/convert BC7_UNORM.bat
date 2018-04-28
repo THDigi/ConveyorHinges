7abd745dfd865ee62c0ea7b9a205f511d8e8f75a
Digi <hunter_digital_sl@yahoo.com>
2018-04-28 15:39:24 +0300

    Tweaked icons conversion from linear to SRGB

--- Textures/Icons/convert BC7_UNORM.bat
+++ Textures/Icons/convert BC7_UNORM.bat
@@ -1,4 +1,4 @@
 echo off
 cls
-C:\Programs\Texconv\texconv.exe *.png -nologo -y -f BC7_UNORM -pmalpha -if LINEAR
+C:\Programs\Texconv\texconv.exe *.png -nologo -y -f BC7_UNORM_SRGB -pmalpha -if LINEAR
 pause
